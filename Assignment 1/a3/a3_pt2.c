9f4bdb2a7adb43c3e9934338691febcdbcfd57fa
adiXcodr <ichbinadittyadey@gmail.com>
2020-09-30 22:23:41 +0530

    Assignment 1

---
+++ Assignment 1/a3/a3_pt2.c
@@ -0,0 +1,14 @@
+//CSB18061
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+
+        int a=4,b=5;
+
+        printf("This is the new program that has occupied the Child Process Space and %d + %d = %d\n",a,b,a+b);
+        exit(0);
+
+}
